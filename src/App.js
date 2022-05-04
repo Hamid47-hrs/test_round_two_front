b26b3ea6548220257f3ffb9a035cd370ce06ad6d
Hamid.hrs <hamid47.hrs@gmail.com>
2022-05-04 18:05:03 +0430

    Add toastify container to App component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
-import Main from './components/main/Main';
+import { ToastContainer } from "react-toastify";
+import Main from "./components/main/Main";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <ToastContainer />
     </div>
   );
 }
